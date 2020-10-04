4c16acc7717c3be05d7c31b3c5ada751a103154d
vladik-cmd <63416337+vladik-cmd@users.noreply.github.com>
2020-10-04 10:41:50 +0300

    header, story, video
    Стилизация блоков header, story, video. Слайдер в  header, фунциональность в video

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,14 @@
 $(function(){
-
+$('.header__slider').slick({
+	arrows: false,
+	dots: true,
+	autoplay: true,
+ 	autoplaySpeed: 2000,
+});
+$('.video-play__icon').click(function(){
+	$('.video-play').addClass('hide');
+	$('.video__wrap').addClass('hide');
+});
 
 	
 });
