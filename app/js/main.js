7e0a369ed906f9902335a3ae243d6d508c1adc2f
vladik-cmd <63416337+vladik-cmd@users.noreply.github.com>
2020-10-05 21:19:17 +0300

    PC версия готова

--- app/js/main.js
+++ app/js/main.js
@@ -5,21 +5,33 @@ $('.header__slider').slick({
 	autoplay: true,
  	autoplaySpeed: 2000,
 });
+
 $('.video-play__icon').click(function(){
 	$('.video-play').addClass('hide');
 	$('.video__wrap').addClass('hide');
 });
+
 $('.quotes__slider-name').slick({
 	fade: true,
 	arrows: false,
   	asNavFor: '.quotes__slider-photo',
 });
+
 $('.quotes__slider-photo').slick({
 	asNavFor: '.quotes__slider-name',
 	slidesToShow: 5,
 	slidesToScroll: 1,
 	prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt=""><img>',
 	nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt=""><img>',
+});
+
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
 });
 
 	
