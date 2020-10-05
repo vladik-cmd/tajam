66f08ade873175f0762184bcfbed244964fef10d
vladik-cmd <63416337+vladik-cmd@users.noreply.github.com>
2020-10-05 18:41:12 +0300

    team+works+quotes

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,18 @@ $('.video-play__icon').click(function(){
 	$('.video-play').addClass('hide');
 	$('.video__wrap').addClass('hide');
 });
+$('.quotes__slider-name').slick({
+	fade: true,
+	arrows: false,
+  	asNavFor: '.quotes__slider-photo',
+});
+$('.quotes__slider-photo').slick({
+	asNavFor: '.quotes__slider-name',
+	slidesToShow: 5,
+	slidesToScroll: 1,
+	prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt=""><img>',
+	nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt=""><img>',
+});
 
 	
 });
